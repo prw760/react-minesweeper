9ff3376e89eb128c428156ce8d3d341b9207acb3
paulrwade <pau2237029@maricopa.edu>
2025-03-23 16:23:12 -0700

    Initial check-in of minesweeper source

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './bomb.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className="courier-prime-regular">Minesweeper is loading...</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
